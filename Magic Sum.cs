a9550464a9fb418fb1f8c430e95e647762ecca85
Djan <124775935+DjanRamadan@users.noreply.github.com>
2023-03-30 17:52:41 +0300

    Add files via upload

---
+++ Magic Sum.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magic_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to magic sum!");
+            Console.WriteLine();
+
+            Console.WriteLine("Enter your numbers: ");
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Console.WriteLine();
+
+            Console.WriteLine("Enter magic sum: ");
+            int sum = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("Pairs of numbers equal to your magic sum: ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i; j < arr.Length; j++)
+                {
+                    if (arr[i] + arr[j] == sum)
+                    {
+                        Console.WriteLine($"{arr[i]} + {arr[j]} = {sum}");
+                    }
+                }
+            }
+        }
+    }
+}
